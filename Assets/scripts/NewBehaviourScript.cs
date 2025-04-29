bc3006e2a6c565a66a5f174af2130d789c2a79c2
micabod <167199490+micabod@users.noreply.github.com>
2025-04-29 08:48:01 -0300

    listo! 💯

--- Assets/scripts/NewBehaviourScript.cs
+++ Assets/scripts/NewBehaviourScript.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public string patente;
+    public int anio;
+    public float km;
+    public int vto;
+    public int hc;
+    float promedio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (patente==("") || anio <1900 || km< 0 || vto <anio || vto > 2025 || hc <5 || hc >100)
+        {
+            Debug.Log("VTV no aprobada");
+            return;
+        }
+        promedio = km / (2025 - anio);
+        if (promedio < 10000)
+        {
+            Debug.Log("VTV otorgada por dos años");
+        }
+        else
+        {
+            Debug.Log("VTV otorgada por un año");
+        }
+  
     }
 
     // Update is called once per frame
